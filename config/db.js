63da0d15b881315592c2bd74729adc28d4be59fb
mayur upadhyay <mayur.upadhyay@mindruby.com>
2023-09-17 12:59:12 +0530

    db url added  added

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const urlString = "mongodb+srv://mustafaabdeali284:mustafa@cluster0.jjhctsx.mongodb.net/bhs?retryWrites=true&w=majority";
 
 const connectDataBase=( )=>{
     console.log(urlString);
-    mongoose.connect(urlString).then(()=>{
+    mongoose.connect('mongodb+srv://mustafaabdeali284:mustafa@cluster0.jjhctsx.mongodb.net/bhs?retryWrites=true&w=majority').then(()=>{
         console.log('connection successful');
     }).catch((err)=>{
         console.log(err);
