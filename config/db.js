380340f5ea0dcc2f1cdcba5e920df36368ce9bbe
mayur upadhyay <mayur.upadhyay@mindruby.com>
2023-09-17 13:49:20 +0530

    urlstring changed

--- config/db.js
+++ config/db.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 
 
 const connectDataBase=( )=>{
-    mongoose.connect('mongodb+srv://mustafaabdeali284:mustafa@cluster0.jjhctsx.mongodb.net/bhs?retryWrites=true&w=majority', {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-    }).then(()=>{
+    mongoose.connect('mongodb+srv://mustafaabdeali284:mustafa@cluster0.jjhctsx.mongodb.net/bhs?retryWrites=true&w=majority').then(()=>{
         console.log('connection successful');
     }).catch((err)=>{
         console.log(err);
