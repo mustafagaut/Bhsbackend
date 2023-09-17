4fb8786930838a50c7b3727550b54fb99185d234
mayur upadhyay <mayur.upadhyay@mindruby.com>
2023-09-17 12:44:33 +0530

    vercel.json added

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const urlString = "mongodb+srv://mustafaabdeali284:mustafa@cluster0.jjhctsx.mongodb.net/bhs?retryWrites=true&w=majority";
 
 const connectDataBase=( )=>{
-    console.log(process.env.DB_URL);
-    mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log(urlString);
+    mongoose.connect(urlString).then(()=>{
         console.log('connection successful');
     }).catch((err)=>{
         console.log(err);
